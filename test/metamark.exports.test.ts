8b45cd62fdd858a86141e1aba359caedc47ef6da
Austin Gatlin <austin@gatlin.io>
2025-03-13 20:22:16 -0700

    improve test coverage for metamark exports

--- test/metamark.exports.test.ts
+++ test/metamark.exports.test.ts
@@ -4,7 +4,9 @@ import m from "../src/index";
 test("metamark exports", () => {
   expect(m.obsidian.vault.process).toBeTypeOf("function");
 
-  expect(m.utility.toSlug).toBeTypeOf("function");
   expect(m.utility.getFileName).toBeTypeOf("function");
   expect(m.utility.getFrontmatterAndMd).toBeTypeOf("function");
+  expect(m.utility.jsonStringify).toBeTypeOf("function");
+  expect(m.utility.toSlug).toBeTypeOf("function");
+  expect(m.utility.writeToFileSync).toBeTypeOf("function");
 });
